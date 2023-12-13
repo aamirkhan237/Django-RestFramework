944f2af3742d6003a1f9e1576674e6600a461498
aamirkhan237 <aamir.4u71@gmail.com>
2023-12-13 14:45:07 +0530

    DjangoModelPermission

--- gs21/api/views.py
+++ gs21/api/views.py
@@ -1,16 +1,19 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 
 #IsAdminUser= staff(True)
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
-    authentication_classes=[BasicAuthentication] 
-    permission_classes=[IsAuthenticated]
-    permission_classes=[IsAdminUser,]  
+    authentication_classes=[SessionAuthentication] 
+    # permission_classes=[IsAuthenticated]
+    # permission_classes=[IsAdminUser,]  
+    # permission_classes=[IsAuthenticatedOrReadOnly] 
+    # permission_classes=[DjangoModelPermissions]  
+    permission_classes=[DjangoModelPermissionsOrAnonReadOnly]  
     # permission_classes=[AllowAny,]  
 
 
