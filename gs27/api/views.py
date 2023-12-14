5c562f3fd0aa3804b19e1d73cf6f7df5a64dc244
aamirkhan237 <aamir.4u71@gmail.com>
2023-12-14 12:53:41 +0530

    Automatic Token Generation by signalmethod When User is Added

--- gs27/api/views.py
+++ gs27/api/views.py
@@ -10,4 +10,9 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     serializer_class=StudentSerializer
     authentication_classes=[] 
     # permission_classes=[IsAuthenticated]
+    # permission_classes=[IsAdminUser,]  
+    # permission_classes=[IsAuthenticatedOrReadOnly] 
+    # permission_classes=[DjangoModelPermissions]  
+    # permission_classes=[DjangoModelPermissionsOrAnonReadOnly]  
+    # permission_classes=[AllowAny,]  
 
