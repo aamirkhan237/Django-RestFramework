b78d516293810e098b0932b8e745c1870fd1f12f
aamirkhan237 <aamir.4u71@gmail.com>
2023-12-19 15:32:03 +0530

    Filtering-Search,Ordering

---
+++ gs34/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .serializers import StudentSerializer
+from rest_framework.generics import ListAPIView
+from .models import Student
+# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+class StudentList(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    filter_backends=[DjangoFilterBackend]
+    # filterset_fields=['city']
+    filterset_fields=['name','city']
+
