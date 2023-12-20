af499b4c26bb6e9a74398dd00937485c9c0f4aa6
aamirkhan237 <aamir.4u71@gmail.com>
2023-12-20 12:46:47 +0530

    serializers relations

--- gs41/api/models.py
+++ gs41/api/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 class Singer(models.Model):
     name=models.CharField(max_length=59)
     gender=models.CharField(max_length=55)
-
+    def __str__(self):
+        return self.name
+    
 class Song(models.Model):
     title=models.CharField(max_length=55)
-    singer=models.ForeignKey(Singer,on_delete=models.CASCADE)
+    singer=models.ForeignKey(Singer,on_delete=models.CASCADE,related_name='song')
     duration=models.IntegerField()
+    def __str__(self):
+        return self.title
+
